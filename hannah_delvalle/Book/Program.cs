3d4cd62fc2f1281524899edef3052269f5e6c523
eru123 <yeoligoakino@gmail.com>
2021-10-21 14:27:31 +0800

    update

---
+++ hannah_delvalle/Book/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Book book = new Book("My Book", "Me", 200);
+        Console.WriteLine(book.name);
+        Console.WriteLine(book.author);
+        Console.WriteLine(book.pages);
+    }
+}
---
+++ hannah_delvalle/Comment/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Comment comment = new Comment("user1", "I Like this book");
+        Console.WriteLine(comment.username);
+        Console.WriteLine(comment.content);
+    }
+}
