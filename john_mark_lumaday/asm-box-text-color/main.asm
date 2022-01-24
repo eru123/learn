8dafd0eb6da96c7e2ae9a1b7fefa3977ff51756e
eru123 <yeoligoakino@gmail.com>
2022-01-24 22:28:55 +0800

    added comments

--- john_mark_lumaday/asm-box-text-color/main.asm
+++ john_mark_lumaday/asm-box-text-color/main.asm
@@ -5,67 +5,81 @@ txt db 10, 13, '    RED', 10, 13, 10, 13, 10, 13, '              GREEN',10, 13, 
 
 .code
 main proc
-    call draw_box
-    call write_text
+    call draw_box               ; call draw_box procedure
+    call write_text             ; call write_text procedure
     
     ; exit
     mov ax, 4c00h
     int 21h
-
     ret
 main endp
 
+; draw_box procedure
 draw_box proc near
     ; set cursor to (0,0)
     mov ah, 6
     
-    ; red box
-    mov al, 0
-    mov bh, 74h
-    mov ch, 0
-    mov cl, 0
-    mov dh, 2d
-    mov dl, 10d
-    int 10h
+    ; draw red box
+    mov al, 0               ; number of characters to write
+    mov bh, 74h             ; set bg color to 7 (gray) and fg color to 4 (red) 
+                            ; h means hexadecimal
+                            ; 7 is the hexadecimal value of gray
+                            ; 4 is the hexadecimal value of red
+    mov ch, 0               ; y coordinate
+    mov cl, 0               ; x coordinate
+    mov dh, 2d              ; height
+    mov dl, 10d             ; width
+    int 10h                 ; bios call or interrupt
+                            ; draw the box
 
-    ; green box
-    mov al, 0
-    mov bh, 72h
-    mov ch, 3d
-    mov cl, 11d
-    mov dh, 5d
-    mov dl, 21d
-    int 10h
+    ; draw green box
+    mov al, 0               ; number of characters to write
+    mov bh, 72h             ; set bg color to 7 (gray) and fg color to 2 (green)
+                            ; h means hexadecimal
+                            ; 7 is the hexadecimal value of gray
+                            ; 2 is the hexadecimal value of green
+    mov ch, 3d              ; y coordinate
+    mov cl, 11d             ; x coordinate
+    mov dh, 5d              ; height
+    mov dl, 21d             ; width
+    int 10h                 ; bios call
+                            ; draw the box
 
-    ; blue box
-    mov al, 0
-    mov bh, 71h
-    mov ch, 6d
-    mov cl, 22d
-    mov dh, 8d
-    mov dl, 32d
-    int 10h
+    ; draw blue box
+    mov al, 0               ; number of characters to write
+    mov bh, 71h             ; set bg color to 7 (gray) and fg color to 1 (blue)
+                            ; h means hexadecimal
+                            ; 7 is the hexadecimal value of gray
+                            ; 1 is the hexadecimal value of blue
+    mov ch, 6d              ; y coordinate
+    mov cl, 22d             ; x coordinate
+    mov dh, 8d              ; height
+    mov dl, 32d             ; width
+    int 10h                 ; bios call
+                            ; draw the box
     
-    ret
+    ret                     ; return
 draw_box endp
 
+; write_text procedure
 write_text proc near
     ; set to text mode
-    mov ah, 3
-    mov al, 0
-    mov bh, 0
-    int 10h
+    mov ah, 3               ; set to text mode
+    mov al, 0               ; number of characters to write
+    mov bh, 0               ; set color to default
+    int 10h                 ; bios call
     
     ; get data
-    mov ax, @data
-    mov ds, ax
+    mov ax, @data           ; get address of data
+    mov ds, ax              ; set ds to data segment
     
     ; print text
-    mov al, 0
-    lea dx, txt
-    mov ah, 09
-    int 21h
+    mov al, 0               ; number of characters to write
+    lea dx, txt             ; get address of txt
+    mov ah, 09              ; print text
+    int 21h                 ; bios call
     
-    ret
-write_text endp
-end main
+    ret                     ; return
+write_text endp 
+end main                    ; end of program
+                            ; main is the entry point
