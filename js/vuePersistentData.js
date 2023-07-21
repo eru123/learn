40e25f49aea68a7a347f366e23f657bbaf0c9049
eru123 <yeoligoakino@gmail.com>
2023-07-21 21:53:36 +0800

    vue 3 state management

---
+++ js/vuePersistentData.js
@@ -0,0 +1,46 @@
+// This script is for persistent state management with localStorage that can be used globally in Vue 3 projects
+import { reactive, watch, toRef } from "vue"
+
+const state = reactive({})
+
+export const usePersistentData = (key, defaultValue) => {
+    if (state[key]) {
+        const data = toRef(state, key)
+        watch(data, () => {
+            window.localStorage.setItem(key, JSON.stringify(data.value))
+        }, { deep: true })
+
+        return data
+    } else if (window.localStorage.getItem(key)) {
+        state[key] = JSON.parse(window.localStorage.getItem(key))
+        const data = toRef(state, key)
+        watch(data, () => {
+            window.localStorage.setItem(key, JSON.stringify(data.value))
+        }, { deep: true })
+
+        return data
+    }
+
+    state[key] = defaultValue
+    window.localStorage.setItem(key, JSON.stringify(defaultValue))
+    const data = toRef(state, key)
+    watch(data, () => {
+        window.localStorage.setItem(key, JSON.stringify(data.value))
+    }, { deep: true })
+
+    return data
+}
+
+export const deletePersistentData = (key) => {
+    delete state[key]
+    window.localStorage.removeItem(key)
+}
+
+export const clearPersistentData = () => {
+    for (const key in state) {
+        delete state[key]
+        window.localStorage.removeItem(key)
+    }
+}
+
+export default usePersistentData
